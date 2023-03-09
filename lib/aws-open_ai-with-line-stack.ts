2d5ea336a6d531fb5d4ff82f70702aa451f999af
nnydtmg <nnydtmg@gmail.com>
2023-03-09 20:52:22 +0900

    first commit

--- lib/aws-open_ai-with-line-stack.ts
+++ lib/aws-open_ai-with-line-stack.ts
@@ -1,16 +1,70 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from "constructs"
+import * as cdk from "aws-cdk-lib"
 
 export class AwsOpenAiWithLineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // DynamoDBテーブル
+    const messagesTable = new cdk.aws_dynamodb.Table(this, "messagesTable", {
+      tableName: "messages",
+      partitionKey: {
+        name: "id",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+      billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+    messagesTable.addGlobalSecondaryIndex({
+      indexName: "userIdIndex",
+      partitionKey: {
+        name: "userId",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsOpenAiWithLineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // LINEとOpenAIの各種シークレット・APIキーをSSMパラメータストアから取得
+    const lineMessagingApiChannelSecret =
+      cdk.aws_ssm.StringParameter.valueForStringParameter(
+        this,
+        "lineMessagingApiChannelSecret"
+      )
+    const lineMessagingApiChannelAccessToken =
+      cdk.aws_ssm.StringParameter.valueForStringParameter(
+        this,
+        "lineMessagingApiChannelAccessToken"
+      )
+    const openAiSecret = cdk.aws_ssm.StringParameter.valueForStringParameter(
+      this,
+      "openAiSecret"
+    )
+
+    // APIGW Lambda関数
+    const apiFn = new cdk.aws_lambda_nodejs.NodejsFunction(this, "apiFn", {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+      entry: "src/lambda/api-handler.ts",
+      environment: {
+        // 環境変数にシークレットとAPIキーをセット
+        CHANNEL_SECRET: lineMessagingApiChannelSecret,
+        CHANNEL_ACCESS_TOKEN: lineMessagingApiChannelAccessToken,
+        OPEN_AI_SECRET: openAiSecret,
+      },
+      bundling: {
+        sourceMap: true,
+      },
+      timeout: cdk.Duration.seconds(29),
+    })
+    messagesTable.grantReadWriteData(apiFn)
+
+    // APIGW
+    const api = new cdk.aws_apigateway.RestApi(this, "api", {
+      deployOptions: {
+        tracingEnabled: true,
+        stageName: "api",
+      },
+    })
+    api.root.addProxy({
+      defaultIntegration: new cdk.aws_apigateway.LambdaIntegration(apiFn),
+    })
   }
 }
